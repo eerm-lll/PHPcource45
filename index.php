1eb984c1b4c8ebca774f458fb16e28018514652f
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 02:14:18 +0300

    14 задание

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
-for ($i = 0; $i < 5; $i++) {
-    echo $i . "<br>";
-}
+<form method="post">
+    <input type="text" name="number">
+    <input type="submit">
+</form>
 
+<?php
+if ($_POST) {
+    for ($i = 1; $i <= $_POST["number"]; $i++) {
+        echo $i . "<br>";
+    }
+}
+?>
