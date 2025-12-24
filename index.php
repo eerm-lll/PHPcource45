eaa6c4c2f9faef3b31728cd1c5e8ffb02ffcf46a
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 02:32:15 +0300

    19 задание

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <form method="post">
-    <label>Enter a country:</label><br>
-    <input type="text" name="country"><br>
-    <input type="submit" value="Submit">
+    <input type="text" name="country">
+    <input type="submit">
 </form>
 
 <?php
 $capitals = array(
     "USA" => "Washington D.C.",
     "Japan" => "Kyoto",
-    "South Korea" => "Seoul",
     "India" => "New Delhi"
 );
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $country = $_POST["country"];
-    $capital = $capitals[$country] ?? "Capital not found.";
+    $capital = $capitals[$country];
     echo "The capital of {$country} is {$capital}.";
 }
 ?>
