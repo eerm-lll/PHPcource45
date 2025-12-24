a4342b7f40be081bb451697a6d38d66baafdfdbc
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 02:14:29 +0300

    15 задание

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<form method="post">
-    <input type="text" name="number">
-    <input type="submit">
-</form>
-
 <?php
-if ($_POST) {
-    for ($i = 1; $i <= $_POST["number"]; $i++) {
-        echo $i . "<br>";
-    }
+$i = 0;
+while ($i < 5) {
+    echo $i . "<br>";
+    $i++;
 }
-?>
