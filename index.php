cf932716dbcd7117aeb34aefbbf654e06364b95e
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 02:48:06 +0300

    25 задание

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
+<form method="post">
+    <input type="text" name="username" placeholder="Username"><br>
+    <input type="text" name="email" placeholder="Email"><br>
+    <input type="submit" value="Submit">
+</form>
+
 <?php
-$username = " Bro Code ";
-$username = strtolower(trim($username));
-echo $username . "<br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
 
-$phone = "123-456-7890";
-$phone = str_replace("-", "", $phone);
-echo $phone;
+    if (empty($username)) {
+        echo "Username is required.<br>";
+    }
+
+    if ($email === false) {
+        echo "Invalid email format.";
+    }
+}
 ?>
-
