a7bb9c5b9164426efde0fe13686944e32cf69c5d
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 02:13:24 +0300

    10 задание

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<form method="post">
-    <input type="text" name="radius">
-    <input type="submit">
-</form>
-
 <?php
-if ($_POST) {
-    $r = $_POST["radius"];
-    echo round(2 * pi() * $r, 2) . "<br>";
-    echo round(pi() * pow($r, 2), 2) . "<br>";
-    echo round((4/3) * pi() * pow($r, 3), 2);
+$age = 25;
+
+if ($age >= 18) {
+    echo "You may enter";
+} else {
+    echo "Access denied";
 }
-?>
+
 
