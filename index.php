9810976b68511fe39b2277f5b27b942faa24e13f
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 02:47:34 +0300

    23 задание

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
-<form method="post">
-    <input type="checkbox" name="foods[]" value="Pizza"> Pizza<br>
-    <input type="checkbox" name="foods[]" value="Hamburger"> Hamburger<br>
-    <input type="checkbox" name="foods[]" value="Taco"> Taco<br>
-    <input type="submit" name="submit" value="Submit">
-</form>
-
 <?php
-if (isset($_POST["submit"])) {
-    if (isset($_POST["foods"])) {
-        foreach ($_POST["foods"] as $food) {
-            echo "You like {$food}<br>";
-        }
-    } else {
-        echo "No food selected.";
-    }
+function happyBirthday($first_name, $age) {
+    echo "Happy birthday dear {$first_name}<br>";
+    echo "You are {$age} years old!<br>";
 }
+
+happyBirthday("SpongeBob", 30);
 ?>
