2499a8b71ea4d4107e6e0ee047baee9a5d57c1a5
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 02:14:09 +0300

    13 задание

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-$grade = "B";
-
-switch ($grade) {
-    case "A":
-        echo "Excellent";
-        break;
-    case "B":
-        echo "Good";
-        break;
-    default:
-        echo "Unknown grade";
+for ($i = 0; $i < 5; $i++) {
+    echo $i . "<br>";
 }
 
