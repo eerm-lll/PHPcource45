07fdeba1009898dc14a36f022042907dfc5eeabb
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 03:47:42 +0300

    35 задание

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+<!-- форма регистрации -->
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+    <h2>Welcome to FakeBook</h2>
+    <label>Username</label><br>
+    <input type="text" name="username"><br>
+    <label>Password</label><br>
+    <input type="password" name="password"><br>
+    <input type="submit" name="submit" value="Register">
+</form>
+
 <?php
 include 'database.php';
-$sql = "SELECT * FROM users WHERE user='SpongeBob'";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    echo 'ID ' . $row['id'] . '<br>';
-    echo 'Username ' . $row['user'] . '<br>';
-} else {
-    echo 'No user found.';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+    
+    if (empty($username)) {
+        echo 'Please enter a username.';
+    } elseif (empty($password)) {
+        echo 'Please enter a password.';
+    } else {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+        try {
+            mysqli_query($conn, $sql);
+            echo 'You are now registered.';
+        } catch (mysqli_sql_exception) {
+            echo 'That username is taken.';
+        }
+        mysqli_close($conn);
+    }
 }
-mysqli_close($conn);
 ?>
+</body>
+</html>
