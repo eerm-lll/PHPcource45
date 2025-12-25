6418d5208441a3a9ab9383f73b6c7435ee7ad55b
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 03:04:20 +0300

    27 задание

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-git add .
-git commit -m "26 задание"
-git push
+<?php
+// cookie 1
+setcookie('favoritefood', 'pizza', time() + 86400); // cookie на 24 часа
+
+if (isset($_COOKIE['favoritefood'])) {
+    echo 'Buy some ' . $_COOKIE['favoritefood'] . '!';
+}
+
+// удалить cookie
+setcookie('favoritefood', '', time() - 3600);
+?>
