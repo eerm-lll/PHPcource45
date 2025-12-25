649cafc0586ac4a72db9ad7d8b0ba3ddf0ba6604
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 03:03:09 +0300

    26 задание

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<form method="post">
-    <input type="text" name="username" placeholder="Username"><br>
-    <input type="text" name="email" placeholder="Email"><br>
-    <input type="submit" value="Submit">
-</form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-
-    if (empty($username)) {
-        echo "Username is required.<br>";
-    }
-
-    if ($email === false) {
-        echo "Invalid email format.";
-    }
-}
-?>
+<!-- index.php -->
+<?php include 'header.html'; ?>
+<h1>Home Page</h1>
+<p>Welcome to the website.</p>
+<?php include 'footer.html'; ?>
