75c2f512d489609240e92874507104457ad167aa
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 03:03:45 +0300

    27 задание

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!-- index.php -->
-<?php include 'header.html'; ?>
-<h1>Home Page</h1>
-<p>Welcome to the website.</p>
-<?php include 'footer.html'; ?>
+git add .
+git commit -m "26 задание"
+git push
