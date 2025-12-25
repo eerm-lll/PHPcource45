459f71a68dba133edf636ba256080a6459a33440
eerm-lll <leraermolaeva09@gmail.com>
2025-12-25 03:05:24 +0300

    29 задание

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-session_start();
-$_SESSION['username'] = 'SpongeBob';
-echo 'Welcome ' . $_SESSION['username'];
-session_destroy();
+$action_file = htmlspecialchars($_SERVER['PHP_SELF']);
+echo '<form action="' . $action_file . '" method="post">';
 ?>
+<!-- форма -->
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    echo 'Form submitted via POST.';
+}
+?>
+>
